1dce7417f1f6e187e0f779e09e718d8d5f8d00e2
Hurious <denny_danh_nguyen@yahoo.com>
2016-04-17 13:54:02 -0500

    Update Dicetest.java

--- Dicetest.java
+++ Dicetest.java
@@ -20,15 +20,26 @@ public class Dicetest {
         Dice dice = new Dice(12);
         Weapons weapons = new Weapons();
         Warrior warrior = new Warrior("Denny");
+        Thief thief = new Thief ("Donovan");
+        Wizard wizard = new Wizard("Dominic");
+
+        System.out.println("Testing: " + warrior.str + ","+ warrior.dex + "," + warrior.con+ "," + warrior.intl);
+        System.out.println("Testing: " + warrior.getMod(warrior.str));
+        System.out.println(dice.randomDiceAttack(warrior.getMod(warrior.str)));
+        System.out.println(dice.randomDiceDamageWarrior(weapons.Club, warrior.getMod(warrior.str)));
+        System.out.println(warrior.name + "," + warrior.health + "," + warrior.AC);
         
-        warrior.setMod(warrior.str);
-        warrior.setHealth(10, 6);
+        System.out.println("Testing: " + thief.str + ","+ thief.dex + "," + thief.con+ "," + thief.intl);
+        System.out.println("Testing: " + thief.getMod(thief.dex));
+        System.out.println(dice.randomDiceAttack(thief.getMod(thief.dex)));
+        System.out.println(dice.randomDiceDamageWarrior(weapons.dagger, thief.getMod(thief.dex)));
+        System.out.println(thief.name + "," + thief.health + "," + thief.AC);
         
-        System.out.println(dice.randomDiceAttack(4));
-        System.out.println(dice.randomDiceDamageWarrior(weapons.Club, warrior.mod));
-        System.out.println(warrior.health);
-        System.out.println(warrior.mod);
-        System.out.println(warrior.name);
+        System.out.println("Testing: " + wizard.str + ","+ wizard.dex + "," + wizard.con+ "," + wizard.intl);
+        System.out.println("Testing: " + wizard.getMod(wizard.intl));
+        System.out.println(dice.randomDiceAttack(wizard.getMod(wizard.intl)));
+        System.out.println(dice.randomDiceDamageWarrior(weapons.staff, warrior.getMod(wizard.intl)));
+        System.out.println(wizard.name + "," + wizard.health + "," + wizard.AC);
       
         
         
