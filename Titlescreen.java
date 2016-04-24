0e7c01621f25b0f4ac5e60f20816156001a861d1
smaddux <smaddux@islander.tamucc.edu>
2016-04-24 12:27:05 -0500

    Update Titlescreen.java

--- Titlescreen.java
+++ Titlescreen.java
@@ -8,9 +8,15 @@ import java.util.ArrayList;
 package testgame;
 import java.util.Scanner;
 import java.io.File;
+import java.io.*;
+
 /**
  *
  * @author Stephen
+ * updates:
+ * access to CharacterCreation for creating a new game
+ * Can now write data to a .dat file
+ * Can now read data from a .dat file
  */
 
 public class Titlescreen {
@@ -19,7 +25,7 @@ public class Titlescreen {
         
     }
     //Handles the options menu
-    public void options()
+    public void options() throws IOException
     {
         String userchoice;
         boolean cases = false;
@@ -59,20 +65,25 @@ public class Titlescreen {
         System.out.println(s);
     }
     
-    public void newGame()
+    public void newGame() throws FileNotFoundException, IOException
     {
         String username;
+        CharacterCreation create = new CharacterCreation();
         Scanner in = new Scanner(System.in);
         sy("Enter your name");
         username = in.nextLine();
         sy(username);
+        create.createCharacter();
+        
     }
+    
+ 
     
     public void loadGame()
     {
         // Directory path for saves
-        String path = "C:\\Users\\dmc\\Documents\\NetBeansProjects\\TestGame\\src\\Saves"; 
- 
+        String path = "C:\\Users\\dmc\\Documents\\NetBeansProjects\\TestGame"; 
+        String userline;
         String files;
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles(); 
@@ -83,9 +94,70 @@ public class Titlescreen {
             if (listOfFiles[i].isFile()) 
             {
                 files = listOfFiles[i].getName();
-                System.out.println(files);
+                if(files.contains("dat")){
+                    System.out.println(files);
+                }
+                
             }
         }
-        sy("Select save");
+        sy("Select save: ");
+        Scanner in = new Scanner(System.in);
+        userline=in.nextLine();
+        restoreState(userline);
+        
+    }
+    //restoreState fetches information from the save file
+    
+    public void restoreState(String userline)
+    {
+        String name =" ";
+        String type =" ";
+        int AC = 0;
+        int exp = 0;
+        int str = 0;
+        int dex = 0;
+        int con = 0;
+        int intl = 0;
+        int attack = 0;
+        int health = 0;
+        int currentHealth = 0;
+        try{
+            FileInputStream saveFile = new FileInputStream(userline);
+            ObjectInputStream save = new ObjectInputStream(saveFile);
+            name = (String) save.readObject();
+            type = (String) save.readObject();
+            AC = (Integer) save.readObject();
+            exp = (Integer) save.readObject();
+            str = (Integer) save.readObject();
+            dex = (Integer) save.readObject();
+            con = (Integer) save.readObject();
+            intl = (Integer) save.readObject();
+            attack = (Integer) save.readObject();
+            health = (Integer) save.readObject();
+            currentHealth = (Integer) save.readObject();
+            
+            save.close();
+        }
+        catch(Exception exc){
+            exc.printStackTrace(); // If there was an error, print the info.
+                        
+        }
+        sy("Character traits\n");
+        sy("Name: \n"+ name);
+        sy("type: \n"+ type);
+        sy("AC: \n"+ AC);
+        sy("exp: \n"+exp);
+        sy("str: \n"+ str);
+        sy("dex: \n"+ dex);
+        sy("con: \n"+ con);
+        sy("intl: \n"+ intl);
+        sy("attack: \n"+ attack);
+        sy("health: \n"+ health);
+        sy("currentHealth: \n"+ currentHealth);
     }
 }
+
+ 
+
+ 
+       
