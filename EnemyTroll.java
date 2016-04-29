922a4d4fde0cf646356f61b13879430f141f8f3f
Hurious <denny_danh_nguyen@yahoo.com>
2016-04-29 16:57:56 -0500

    Update EnemyTroll.java

--- EnemyTroll.java
+++ EnemyTroll.java
@@ -9,5 +9,18 @@
  * @author Denny
  */
 public class EnemyTroll {
-    
+    public EnemyTroll()
+    {
+        super();
+        this.type = "Troll";
+        this.AC = 16;
+        this.exp = 25;
+        this.str = 18; //Trolls mod stat, getMod(str); 
+        this.dex = 12;
+        this.con = 15;
+        this.intl = 8;
+        super.setMod(str);
+        super.setHealth(40);
+        
+    }
 }
